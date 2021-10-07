68e134cca91cd97d9cdaded00c8a52fa348ae73c
crkeg234 <89236516+crkeg234@users.noreply.github.com>
2021-10-07 18:58:04 -0500

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -23,7 +23,7 @@ Owner_info_msg = f"""
 
 <strong>Message Forwards</strong> - {udB.get("PMBOT")}
 
-<strong>Ultroid <a href=https://github.com/TeamUltroid/Ultroid>[v{ultroid_version}]</a>, powered by @TeamUltroid</strong>
+<strong>Ultroid <a href=https://github.com/TeamUltroid/Ultroid>[v{ultroid_version}]</a>, By Bot {OWNER_NAME}</strong>
 """
 
 _settings = [
